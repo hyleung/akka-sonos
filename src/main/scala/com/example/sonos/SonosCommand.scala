9cbe4935baa5d43c33faacbb5084d4bb4c8ae32a
Ho Yan Leung <hy.leung@gmail.com>
2015-07-12 09:50:38 -0700

    Initial start on SOAP stuff

---
+++ src/main/scala/com/example/sonos/SonosCommand.scala
@@ -0,0 +1,64 @@
+package com.example.sonos
+
+import scala.xml.{MetaData, NamespaceBinding, Elem}
+
+/**
+ * Case class for constructing the SOAP calls to the SONOS api.
+ *
+ * == Message format ==
+ *
+ * The message format for the SOAP calls should look something like this.
+ * If the SOAP action takes no parameters, the `<s:Body>` element can be empty.
+ *
+ * Service types are shown in the `device_description.xml` response.
+ *
+ * <?xml version="1.0"?>
+ *	<s:Envelope
+ *		xmlns:s="http://schemas.xmlsoap.org/soap/envelope/"
+ *		s:encodingStyle="http://schemas.xmlsoap.org/soap/encoding/">
+ *		<s:Body>
+ *			<u:actionName
+ *				xmlns:u="urn:schemas-upnp-org:service:serviceType:v">
+ *			</u:actionName>
+ *		</s:Body>
+ *	</s:Envelope>
+ *
+ *
+ * @param serviceType the service type (e.g. `ZoneGroupTopology`)
+ * @param version the service version (e.g. 1)
+ * @param action the as described in the scpd (e.g. /xml/ZoneGroupTopology1.xml)
+ * @param arguments any arguments for the SOAP action
+ */
+case class SonosCommand(serviceType:String, version:Int, action:String, arguments:Map[String,String]) {
+	/**
+	 * SOAPACTION header for the http POST call to the SOAP endpoint.
+	 */
+	val actionHeader = s"SOAPACTION:$serviceTypeNamespace:$version#$action"
+
+	/**
+	 * xml namespace for the SOAP call
+	 */
+	lazy val serviceTypeNamespace = s"urn:schemas-upnp-org:service:serviceType:$serviceType"
+
+
+	/**
+	 * XML template for the SOAP (ugh) message.
+	 */
+	private val envelope = <s:Envelope
+	xmlns:s="http://schemas.xmlsoap.org/soap/envelope/"
+	s:encodingStyle="http://schemas.xmlsoap.org/soap/encoding/">
+		<s:Body>
+		</s:Body>
+	</s:Envelope>
+
+	/**
+	 * Return the SOAP message for the command
+	 * @return XML SOAP message
+	 */
+	def soapXml:Elem = {
+		val children = arguments.foldRight(List.empty[Elem])((p,acc) => Elem(null,p._1,xml.Null,xml.TopScope,minimizeEmpty = true, xml.Text(p._2)) +: acc)
+		Elem("u",action, xml.Null ,NamespaceBinding("u",serviceTypeNamespace, null), minimizeEmpty = true, children:_*)
+	}
+
+
+}
---
+++ src/test/scala/com/example/sonos/SonosCommandSpec.scala
@@ -0,0 +1,38 @@
+package com.example.sonos
+
+import org.scalatest.{FlatSpec, Matchers}
+
+import scala.xml.Node
+
+/**
+ * Created with IntelliJ IDEA.
+ * Date: 15-07-11
+ * Time: 3:00 PM
+ * To change this template use File | Settings | File Templates.
+ */
+class SonosCommandSpec extends FlatSpec with Matchers {
+	behavior of "SonosCommand"
+	it should "return correct header" in {
+		val command = SonosCommand("ZoneGroupTopology",1,"GetZoneGroupState",Map.empty)
+		command.actionHeader should be ("SOAPACTION:urn:schemas-upnp-org:service:serviceType:ZoneGroupTopology:1#GetZoneGroupState")
+	}
+	it should "have empty body" in {
+		val command = SonosCommand("SomeEndpoint",1,"SomeAction",Map.empty)
+		val soapXml = command.soapXml
+		(soapXml \\ "foo").headOption should be (None)
+	}
+	it should "have soap body with args" in {
+		val command = SonosCommand("SomeEndpoint",1,"SomeAction",Map("foo" -> "bar","fizz" -> "buzz"))
+		val soapXml = command.soapXml
+		(soapXml \\ "foo").headOption should be (Some(<foo>bar</foo>))
+		(soapXml \\ "fizz").headOption should be (Some(<fizz>buzz</fizz>))
+	}
+	it should "have action element" in {
+		val command = SonosCommand("SomeEndpoint",1,"SomeAction",Map("foo" -> "bar","fizz" -> "buzz"))
+		val soapXml = command.soapXml
+		println(soapXml)
+		val headOption: Option[Node] = (soapXml \\ "SomeAction").headOption
+		headOption.nonEmpty should be (true)
+		headOption.get.namespace should be (command.serviceTypeNamespace)
+	}
+}
