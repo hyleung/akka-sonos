161a7e4aaa8c9c9879956cc51fc5f44c8ca21502
Ho Yan Leung <hy.leung@gmail.com>
2015-08-29 11:28:22 -0700

    Refactors out UDP discovery into trait
    Fixes up tests to use stubbed or mocked udp discovery

--- src/main/scala/com/example/actors/DiscoveryActor.scala
+++ src/main/scala/com/example/actors/DiscoveryActor.scala
@@ -23,38 +23,37 @@ import scala.language.postfixOps
  * Time: 9:54 PM
  * To change this template use File | Settings | File Templates.
  */
-class DiscoveryActor(address:String, interface:Option[String], port:Int, udp:ActorRef) extends Actor{
+class DiscoveryActor(address: String, interface: Option[String], port: Int, udp: ActorRef)
+	extends Actor with SSDPDiscoveryClient {
 	val log = Logging(context.system, this)
 	var broadcastCount = 0
-	val SEARCH = SSDPDiscoveryRequest(Map(
-	"HOST" -> "239.255.255.250:1900",
-	"MAN" -> "\"ssdp:discover\"",
-	"MX" -> "1",
-	"ST" -> " urn:schemas-upnp-org:device:ZonePlayer:1"
-	)).serialize
+
 	val opts = List(InetV4ProtocolFamily(), MulticastGroup(address, interface))
+
 	//send a message to bind this actor to the socket address
-	def receive:Receive = {
+	def receive: Receive = {
 		case StartDiscovery() =>
 			val socketAddress = new InetSocketAddress(InetAddress.getLocalHost, port)
 			val remote = new InetSocketAddress(address, 1900)
-			udp ! Udp.Bind(self, socketAddress,opts)
+			udp ! Udp.Bind(self, socketAddress, opts)
 			context.become(awaitUdpBind(sender(), remote))
 	}
-	def awaitUdpBind(s:ActorRef, remote:InetSocketAddress): Receive = {
+
+	def awaitUdpBind(s: ActorRef, remote: InetSocketAddress): Receive = {
 		case Udp.Bound(local) =>
 			log.info(s"Bound to $local, awaiting discovery...")
-			sendSearchDatagram(sender(),remote)
+			sendSearchDatagram(sender(), remote)
 			context.become(ready(s, remote))
-			context.system.scheduler.scheduleOnce(1000 milli,self, OnTimeout())
+			context.system.scheduler.scheduleOnce(1000 milli, self, OnTimeout())
 	}
 
-	def ready(sender:ActorRef, socket:InetSocketAddress):Receive = {
+	def ready(sender: ActorRef, socket: InetSocketAddress): Receive = {
 		case OnTimeout() =>
 			sendSearchDatagram(sender, socket)
+			broadcastCount += 1
 			//schedule the next timeout
-			context.system.scheduler.scheduleOnce(1000 milli,self, OnTimeout())
-		case Udp.Received(data,socketAddress) =>
+			context.system.scheduler.scheduleOnce(1000 milli, self, OnTimeout())
+		case Udp.Received(data, socketAddress) =>
 			val response = data.decodeString("UTF-8")
 			//attempt to deserialize as a Discovery notification
 			if (response.contains("Sonos")) {
@@ -70,37 +69,27 @@ class DiscoveryActor(address:String, interface:Option[String], port:Int, udp:Act
 				}
 			}
 
-		case Udp.Unbind =>  sender ! Udp.Unbind
+		case Udp.Unbind => sender ! Udp.Unbind
 		case Udp.Unbound => context.stop(self)
 	}
 
-	def sendSearchDatagram(actor: ActorRef,  socket:InetSocketAddress): Unit = {
-		val data: ByteString = ByteString(SEARCH, "UTF-8")
-		actor ! Udp.Send(data, socket)
-		actor ! Udp.Send(data, socket)
-		actor ! Udp.Send(data, socket)
-		broadcastCount += 1
-	}
-
-	def times(f:() => Unit, repeat:Int): Unit = {
-		(1 to repeat) foreach( _ => f)
-	}
-
 }
+
 //UDP Multi-cast
 final case class InetV4ProtocolFamily() extends DatagramChannelCreator {
 	@throws[Exception](classOf[Exception])
 	override def create(): DatagramChannel = DatagramChannel.open(StandardProtocolFamily.INET)
 }
+
 //Multi-cast group settings
-final case class MulticastGroup(address:String, interface:Option[String]) extends SocketOptionV2 with LazyLogging {
+final case class MulticastGroup(address: String, interface: Option[String]) extends SocketOptionV2 with LazyLogging {
 	override def afterBind(socket: DatagramSocket): Unit = {
 		val group = InetAddress.getByName(address)
-		val networkInterface:Seq[NetworkInterface] = interface match {
+		val networkInterface: Seq[NetworkInterface] = interface match {
 			case Some(iface) => Seq(NetworkInterface.getByName(iface))
 			case None =>
 				import scala.collection.JavaConversions._
-				val interfaces = NetworkInterface.getNetworkInterfaces filter  { i => i.isUp && i.supportsMulticast()}
+				val interfaces = NetworkInterface.getNetworkInterfaces filter { i => i.isUp && i.supportsMulticast() }
 				interfaces.toSeq
 		}
 		networkInterface foreach { interface =>
@@ -113,6 +102,23 @@ final case class MulticastGroup(address:String, interface:Option[String]) extend
 
 
 object DiscoveryActor {
-	def props(address:String, interface:String, port:Int, udp:ActorRef) = Props(new DiscoveryActor(address, Some(interface),port, udp))
-	def props(address:String, port:Int, udp:ActorRef) = Props(new DiscoveryActor(address, None, port, udp))
+	def props(address: String, interface: String, port: Int, udp: ActorRef) = Props(new DiscoveryActor(address, Some(interface), port, udp))
+
+	def props(address: String, port: Int, udp: ActorRef) = Props(new DiscoveryActor(address, None, port, udp))
+}
+
+trait SSDPDiscoveryClient {
+	val SEARCH = SSDPDiscoveryRequest(Map(
+		"HOST" -> "239.255.255.250:1900",
+		"MAN" -> "\"ssdp:discover\"",
+		"MX" -> "1",
+		"ST" -> " urn:schemas-upnp-org:device:ZonePlayer:1"
+	)).serialize
+
+	def sendSearchDatagram(actor: ActorRef, socket: InetSocketAddress): Unit = {
+		val data: ByteString = ByteString(SEARCH, "UTF-8")
+		actor ! Udp.Send(data, socket)
+		actor ! Udp.Send(data, socket)
+		actor ! Udp.Send(data, socket)
+	}
 }
--- src/test/scala/com/example/actors/DiscoveryActorSpec.scala
+++ src/test/scala/com/example/actors/DiscoveryActorSpec.scala
@@ -2,60 +2,86 @@ package com.example.actors
 
 import java.net.InetSocketAddress
 
-import akka.actor.ActorSystem
+import akka.actor.{ActorRef, ActorSystem}
 import akka.io.Udp
 import akka.testkit._
 import akka.util.ByteString
 import com.example.protocol.DiscoveryProtocol._
 import com.example.ssdp.{SSDPDatagram, SSDPDiscoveryNotification, SSDPDiscoveryRequest}
+import org.scalamock.scalatest.MockFactory
 import org.scalatest.{BeforeAndAfterAll, Matchers, WordSpecLike}
 
 import scala.concurrent.duration._
 import scala.language.postfixOps
-class DiscoveryActorSpec(_system: ActorSystem) extends TestKit(_system) with ImplicitSender
-  with WordSpecLike with Matchers with BeforeAndAfterAll {
- 
-  def this() = this(ActorSystem("DiscoveryActorSpec"))
- 
-  override def afterAll {
-    TestKit.shutdownActorSystem(system)
-  }
 
-  "DiscoveryActor" must {
-    val udp = TestProbe()
-    val actor = TestActorRef(DiscoveryActor.props("239.255.255.250","en0",1900,udp.ref))
-    val local = InetSocketAddress.createUnresolved("localhost",1900)
-    val remote = InetSocketAddress.createUnresolved("remote",1900)
-    "perform UDP bind on StartDiscovery" in {
-      actor ! StartDiscovery()
-      val options = udp.expectMsgPF(){
-        case Udp.Bind(_,_,o) => o
-      }
-      assert(options.exists(o => o.isInstanceOf[MulticastGroup]))
-      assert(options.exists(o => o.isInstanceOf[InetV4ProtocolFamily]))
-    }
-    "then send multi-cast on bind" in {
-      actor ! Udp.Bound(local)
-      //upon binding, discovery requests are sent back to the sender of the Udp.Bound message
-      val discoveryRequests = receiveWhile(100 millis, 100 millis,10){
-        case Udp.Send(b,_,_) => SSDPDatagram.deserialize[SSDPDiscoveryRequest](b.decodeString("UTF-8")).isDefined
-      }
-      assert(discoveryRequests.size == 3)
-    }
-    "then respond with DiscoveryComplete after SSDP notification received" in {
-      val data ="""HTTP/1.1 200 OK
+class DiscoveryActorSpec(_system: ActorSystem) extends TestKit(_system) with ImplicitSender
+with WordSpecLike
+with Matchers
+with MockFactory
+with BeforeAndAfterAll {
+
+	def this() = this(ActorSystem("DiscoveryActorSpec"))
+
+	override def afterAll {
+		TestKit.shutdownActorSystem(system)
+	}
+
+	"DiscoveryActor" must {
+		val udp = TestProbe()
+		val mock = mockFunction[Unit]
+		val actor = TestActorRef(new TestDiscoveryActor(mock, udp.ref))
+		val local = InetSocketAddress.createUnresolved("localhost", 1900)
+		val remote = InetSocketAddress.createUnresolved("remote", 1900)
+		"perform UDP bind on StartDiscovery" in {
+			actor ! StartDiscovery()
+			val options = udp.expectMsgPF() {
+				case Udp.Bind(_, _, o) => o
+			}
+			assert(options.exists(o => o.isInstanceOf[MulticastGroup]))
+			assert(options.exists(o => o.isInstanceOf[InetV4ProtocolFamily]))
+		}
+		"then send multi-cast on bind" in {
+			//upon binding, discovery requests are sent back to the sender of the Udp.Bound message
+			mock.expects().atLeastOnce()
+			actor ! Udp.Bound(local)
+		}
+		"then respond with DiscoveryComplete after SSDP notification received" in {
+			val data = """HTTP/1.1 200 OK
 			 CACHE-CONTROL: max-age = 1800
 			 LOCATION: http://192.168.1.70:1400/xml/device_description.xml
 			 ST: urn:schemas-upnp-org:device:ZonePlayer:1
 			 USN: uuid:RINCON_B8E93781D11001400::urn:schemas-upnp-org:device:ZonePlayer:1
 			 X-RINCON-BOOTSEQ: 28
 			 X-RINCON-HOUSEHOLD: Sonos_iROH6kmkXYSpfYZTTyCYZMC6jH"""
-      actor ! Udp.Received(ByteString(data,"UTF-8"), local)
-      expectMsgPF(){
-        case DiscoveryComplete(location) => location should be ("http://192.168.1.70:1400/xml/device_description.xml")
-        case _ => fail()
-      }
-    }
-  }
+			actor ! Udp.Received(ByteString(data, "UTF-8"), local)
+			expectMsgPF() {
+				case DiscoveryComplete(location) => location should be("http://192.168.1.70:1400/xml/device_description.xml")
+				case _ => fail()
+			}
+		}
+	}
+	"DiscoveryActor" must {
+		"re-send search datagram on timeout" in {
+			val udp = TestProbe()
+			val actor = TestActorRef(new TestDiscoveryActor(stubFunction[Unit], udp.ref))
+			val local = InetSocketAddress.createUnresolved("localhost", 1900)
+			val remote = InetSocketAddress.createUnresolved("remote", 1900)
+			actor ! StartDiscovery()
+			actor ! Udp.Bound(local)
+			actor ! OnTimeout()
+		}
+	}
+
+	trait MockSsdpClient extends SSDPDiscoveryClient {
+		this: TestDiscoveryActor =>
+		val fake: () => Unit
+
+		override def sendSearchDatagram(actor: ActorRef, socket: InetSocketAddress): Unit = fake()
+	}
+
+	class TestDiscoveryActor(f: () => Unit, udp: ActorRef)
+		extends DiscoveryActor("239.255.255.250", Some("en0"), 1900, udp) with MockSsdpClient {
+		override val fake: () => Unit = f
+	}
 
 }
