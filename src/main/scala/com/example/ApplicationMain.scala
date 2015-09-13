e6c6c3a2d96b486334e02116d1c57af56f589653
Ho Yan Leung <hy.leung@gmail.com>
2015-09-13 09:03:02 -0700

    Refactors out separate Server/Protocol trait

--- src/main/scala/com/example/ApplicationMain.scala
+++ src/main/scala/com/example/ApplicationMain.scala
@@ -1,40 +1,14 @@
 package com.example
 
-import java.util.concurrent.TimeUnit
-
-import akka.actor.{ActorSystem, Props}
 import akka.http.scaladsl.Http
-import akka.http.scaladsl.marshallers.sprayjson.SprayJsonSupport._
-import akka.http.scaladsl.model.Uri
-import akka.http.scaladsl.server.Directives._
-import akka.pattern.ask
-import akka.stream.ActorMaterializer
-import akka.util.Timeout
-import com.example.actors.ApiBridge
-import com.example.protocol.SonosProtocol.{ZoneResponse, ZoneQuery}
-import com.example.sonos.{ZoneGroup, ZoneGroupMember}
+import com.example.server.Server
 import com.typesafe.scalalogging.LazyLogging
-import spray.json._
 
 import scala.concurrent.ExecutionContext.Implicits.global
 import scala.io.StdIn
 import scala.language.postfixOps
 
-object ApplicationMain extends App with LazyLogging with Protocols {
-	implicit val system = ActorSystem("SonosApiBridge")
-	implicit val materializer = ActorMaterializer()
-	implicit val timeout:Timeout = Timeout(5000, TimeUnit.MILLISECONDS)
-	val apiActor = system.actorOf(Props[ApiBridge])
-	val route =
-		path("") {
-			get {
-				complete {
-					(apiActor ? ZoneQuery()).map[JsValue] {
-						case ZoneResponse(g) => g.toJson
-					}
-				}
-			}
-		}
+object ApplicationMain extends App with LazyLogging with Server {
 	val bindingFuture = Http().bindAndHandle(route, "localhost", 8080)
 	println(s"Server online at http://localhost:8080/\nPress RETURN to stop...")
 	StdIn.readLine()
@@ -43,13 +17,4 @@ object ApplicationMain extends App with LazyLogging with Protocols {
 		.flatMap(_.unbind())
 		.onComplete(_ => system.terminate())
 
-}
-
-trait Protocols extends DefaultJsonProtocol {
-	implicit object uriFormat extends RootJsonFormat[Uri] {
-		override def write(obj: Uri): JsValue = JsString(obj.toString())
-		override def read(json: JsValue): Uri = Uri(json.toString())
-	}
-	implicit val zoneGroupMemberFormat:RootJsonFormat[ZoneGroupMember] = jsonFormat(ZoneGroupMember,"name", "uri")
-	implicit val zoneGroupFormat:RootJsonFormat[ZoneGroup] = jsonFormat(ZoneGroup,"zone_members")
 }
---
+++ src/main/scala/com/example/server/Server.scala
@@ -0,0 +1,49 @@
+package com.example.server
+
+import java.util.concurrent.TimeUnit
+
+import akka.actor.{Props, ActorSystem}
+import akka.http.scaladsl.model.Uri
+import akka.http.scaladsl.server.Directives._
+import akka.stream.ActorMaterializer
+import akka.util.Timeout
+import com.example.actors.ApiBridge
+import com.example.protocol.SonosProtocol.{ZoneResponse, ZoneQuery}
+import com.example.sonos.{ZoneGroup, ZoneGroupMember}
+import spray.json._
+import akka.pattern.ask
+import akka.http.scaladsl.marshallers.sprayjson.SprayJsonSupport._
+import scala.concurrent.ExecutionContext.Implicits.global
+
+/**
+ * Created with IntelliJ IDEA.
+ * Date: 15-09-13
+ * Time: 8:52 AM
+ * To change this template use File | Settings | File Templates.
+ */
+trait Server extends Protocols {
+	implicit val system = ActorSystem("SonosApiBridge")
+	implicit val materializer = ActorMaterializer()
+	implicit val timeout:Timeout = Timeout(5000, TimeUnit.MILLISECONDS)
+	val apiActor = system.actorOf(Props[ApiBridge])
+	val route =
+		path("") {
+			get {
+				complete {
+					(apiActor ? ZoneQuery()).map[JsValue] {
+						case ZoneResponse(g) => g.toJson
+					}
+				}
+			}
+		}
+}
+
+trait Protocols extends DefaultJsonProtocol {
+	implicit object uriFormat extends RootJsonFormat[Uri] {
+		override def write(obj: Uri): JsValue = JsString(obj.toString())
+		override def read(json: JsValue): Uri = Uri(json.toString())
+	}
+	implicit val zoneGroupMemberFormat:RootJsonFormat[ZoneGroupMember] = jsonFormat(ZoneGroupMember,"name", "uri")
+	implicit val zoneGroupFormat:RootJsonFormat[ZoneGroup] = jsonFormat(ZoneGroup,"zone_members")
+}
+
